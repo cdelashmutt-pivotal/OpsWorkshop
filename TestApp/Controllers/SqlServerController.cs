465fe84a4c90aa1db3030ed1e09ef56201e76aa1
Chris DeLashmutt <cdelashmutt@pivotal.io>
2018-04-30 23:05:08 -0400

    Better SQL Server Binding Detection

--- TestApp/Controllers/SqlServerController.cs
+++ TestApp/Controllers/SqlServerController.cs
@@ -1,20 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.CloudFoundry.Connector;
+using Steeltoe.CloudFoundry.Connector.Services;
 using System;
+using System.Data.Common;
 using System.Linq;
 
 namespace TestApp.Controllers
 {
     public class SqlServerController : Controller
     {
-        public IActionResult Index([FromServices] TestContext context)
+        TestContext _context;
+        bool _sqlEnabled = false;
+        ILogger<SqlServerController> _logger;
+
+        public SqlServerController([FromServices] TestContext context, [FromServices] ILogger<SqlServerController> logger, IConfiguration config)
         {
-            var connection = context.Database.GetDbConnection();
-            ViewData["sqlEnabled"] = (connection.Database != null && connection.Database != "");
+            _context = context;
+            _logger = logger;
+            SqlServerServiceInfo si = config.GetSingletonServiceInfo<SqlServerServiceInfo>();
+            _sqlEnabled = si != null;
+
+            if(!_sqlEnabled)
+            {
+                _logger.LogTrace("No bound SQL Database detected");
+            }
+            else
+            {
+                _logger.LogTrace("Found SQL Database: {Host:{0}, Port:{1}, User: {2}", si.Host, si.Port, si.UserName);
+            }
+        }
+        public IActionResult Index()
+        {
+            DbConnection connection = _context.Database.GetDbConnection();
+            ViewData["sqlEnabled"] = _sqlEnabled;
             if ((bool)ViewData["sqlEnabled"])
             {
-                Console.WriteLine($"Retrieving data from {connection.DataSource}/{connection.Database}");
-                return View(context.TestData.ToList());
+                _logger.LogTrace($"Retrieving data from {connection.DataSource}/{connection.Database}");
+                return View(_context.TestData.ToList());
             } else
             {
                 return View();
