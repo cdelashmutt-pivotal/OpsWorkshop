f474d645229f58cb06bdbfb85857af5a6948faa8
Chris DeLashmutt <cdelashmutt@pivotal.io>
2018-05-02 23:27:33 -0400

    Add in a Fibonacci Sequence Generator to generate load

---
+++ TestApp/Controllers/FibonacciController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        public static BigInteger Fibonacci(int n)
+        {
+            BigInteger a = 0;
+            BigInteger b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                BigInteger temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(int quantity)
+        {
+            BigInteger calculated = 0;
+            for(int i = 0; i <= quantity; i++)
+            {
+                calculated = Fibonacci(i);
+            }
+            ViewData["value"] = calculated;
+            return View("Index");
+        }
+    }
+}
